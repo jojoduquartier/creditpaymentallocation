3e5f880f959d766b1db23118bcd2300ca408cf9e
Josiah Hounyo <josiahhounyo@gmail.com>
2019-07-24 23:59:08 -0500

    testing hosted page

--- main.py
+++ main.py
@@ -13,9 +13,7 @@ from starlette.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 origins = [
-    "http:localhost",
-    "http:localhost:8080",
-    "http://0.0.0.0:1234"
+    "https://pymtallocation.herokuapp.com"
 ]
 
 # for testing locally
